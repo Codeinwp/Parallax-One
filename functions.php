aed6e3a4bd5ffc7f6938264a4f5dfd35a0e7c356
claudiuvertistudio <claudiu.buruiana@codeinwp.com>
2015-06-08 11:43:39 +0300

    Sidebar style

--- functions.php
+++ functions.php
@@ -103,8 +103,8 @@ function parallax_one_widgets_init() {
 			'description'   => '',
 			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h1 class="widget-title">',
-			'after_title'   => '</h1>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2><div class="colored-line-left"></div><div class="clearfix widget-title-margin"></div>',
 		)
 	);
 	

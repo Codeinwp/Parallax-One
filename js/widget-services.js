24aac71a9bb843f4d6c7b8d294a12ff8315b9083
cristian-ungureanu <cristian@themeisle.com>
2015-06-15 17:17:00 +0300

    update to #48

--- js/widget-services.js
+++ js/widget-services.js
@@ -1,7 +1,6 @@
 function media_upload(button_class) {
 	
 	jQuery('body').on('click', button_class, function(e) {
-		
 		var button_id ='#'+jQuery(this).attr('id');
 		var display_field = jQuery(this).parent().children('input:text');
 		var _custom_media = true;
@@ -20,13 +19,13 @@ function media_upload(button_class) {
 							display_field.val(attachment.sizes.thumbnail.url);
 							break;
 						case 'parallax_one_team':
-							display_field.val(attachment.sizes.team.url);
+							display_field.val(attachment.sizes.parallax_one_team.url);
 							break
 						case 'parallax_one_services':
-							display_field.val(attachment.sizes.services.url);
+							display_field.val(attachment.sizes.parallax_one_services.url);
 							break
 						case 'parallax_one_customers':
-							display_field.val(attachment.sizes.customers.url);
+							display_field.val(attachment.sizes.parallax_one_customers.url);
 							break;
 						default:
 							display_field.val(attachment.url);

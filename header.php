1013ed8ded600b43e91a9640fad5b085a5eb31bf
cristian-ungureanu <cristian@themeisle.com>
2015-06-18 15:17:25 +0300

    this fixes #58 - added parallax_one_only_customizer in style.css

--- header.php
+++ header.php
@@ -15,11 +15,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-<style>
-	.paralax_one_only_customizer{
-		display:none !important;
-	}
-</style>
 <?php wp_head(); ?>
 </head>
 

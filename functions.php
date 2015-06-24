9eefd46d9e77a35f6baa47d8ce6c10d1bc7dbdda
cristian-ungureanu <cristian@themeisle.com>
2015-06-24 14:21:04 +0300

    #74 Include intergeo with tgm

--- functions.php
+++ functions.php
@@ -467,3 +467,61 @@ function remove_class_function( $classes ) {
 
 	// add this code directly, no action needed
 	remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
+
+
+/* tgm-plugin-activation */
+require_once get_template_directory() . '/class-tgm-plugin-activation.php';
+
+
+add_action( 'tgmpa_register', 'parallax_one_register_required_plugins' );
+function parallax_one_register_required_plugins() {
+	
+		$plugins = array(
+			array(
+	 
+				'name'      => 'Intergeo Maps - Google Maps Plugin',
+	 
+				'slug'      => 'intergeo-maps',
+	 
+				'required'  => false,
+	 
+			)
+		);
+	
+	
+ 
+	$theme_text_domain = 'parallax-one';
+	
+	$config = array(
+        'default_path' => '',                      
+        'menu'         => 'tgmpa-install-plugins', 
+        'has_notices'  => true,                   
+        'dismissable'  => true,                  
+        'dismiss_msg'  => '',                   
+        'is_automatic' => false,                 
+        'message'      => '',     
+        'strings'      => array(
+            'page_title'                      => __( 'Install Required Plugins', $theme_text_domain ),
+            'menu_title'                      => __( 'Install Plugins', $theme_text_domain ),
+            'installing'                      => __( 'Installing Plugin: %s', $theme_text_domain ), 
+            'oops'                            => __( 'Something went wrong with the plugin API.', $theme_text_domain ),
+            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ),
+            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ),
+            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ),
+            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ),
+            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ),
+            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), 
+            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), 
+            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), 
+            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
+            'return'                          => __( 'Return to Required Plugins Installer', $theme_text_domain ),
+            'plugin_activated'                => __( 'Plugin activated successfully.', $theme_text_domain ),
+            'complete'                        => __( 'All plugins installed and activated successfully. %s', $theme_text_domain ), 
+            'nag_type'                        => 'updated'
+        )
+    );
+ 
+	tgmpa( $plugins, $config );
+ 
+}

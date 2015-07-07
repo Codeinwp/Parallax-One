2bbb6b4235f5d8467ad0fcd90bdab1142a7f4070
cristian-ungureanu <cristian@themeisle.com>
2015-07-07 17:10:19 +0300

    this fixes #115 - change frontpage

--- front-page.php
+++ front-page.php
@@ -1,34 +1,36 @@
 <?php
-if ( get_option( 'show_on_front' ) == 'page' ) {
-} else {
-/*TODO*/
-get_header(); 
 
-
-	include_once get_template_directory() . "/sections/parallax_one_header_section.php";
-?>
-	</div>
-	<!-- /END COLOR OVER IMAGE -->
-</header>
-<!-- /END HOME / HEADER  -->
-
-<div class="content-wrap">
-
-<?php
+if ( 'posts' == get_option( 'show_on_front' ) ) {
 	
-	$sections_array = apply_filters("parallax_pro_sections_filter",array('sections/parallax_one_logos_section','sections/parallax_one_our_services_section','sections/parallax_one_our_story_section','sections/parallax_one_our_team_section','sections/parallax_one_happy_customers_section','sections/parallax_one_ribbon_section','sections/parallax_one_latest_news_section','sections/parallax_one_contact_info_section','sections/parallax_one_map_section'));
-	
-	if(!empty($sections_array)){
-		foreach($sections_array as $section){
-			include_once get_template_directory() . "/". $section.".php";
+	get_header(); 
+
+
+		include_once get_template_directory() . "/sections/parallax_one_header_section.php";
+	?>
+		</div>
+		<!-- /END COLOR OVER IMAGE -->
+	</header>
+	<!-- /END HOME / HEADER  -->
+
+	<div class="content-wrap">
+
+	<?php
+
+		$sections_array = apply_filters("parallax_pro_sections_filter",array('sections/parallax_one_logos_section','sections/parallax_one_our_services_section','sections/parallax_one_our_story_section','sections/parallax_one_our_team_section','sections/parallax_one_happy_customers_section','sections/parallax_one_ribbon_section','sections/parallax_one_latest_news_section','sections/parallax_one_contact_info_section','sections/parallax_one_map_section'));
+
+		if(!empty($sections_array)){
+			foreach($sections_array as $section){
+				include_once get_template_directory() . "/". $section.".php";
+			}
 		}
-	}
-?>
+	?>
 
-</div><!-- .content-wrap -->
+	</div><!-- .content-wrap -->
 
-<?php 
+	<?php 
 
-get_footer();
+	get_footer();
+} else {
+	include( get_page_template() );
 }
 ?>

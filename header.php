48da47a5e10f1bd76bc73a326d0dccfc1b47084d
cristian-ungureanu <cristian@themeisle.com>
2015-07-08 12:37:09 +0300

    #125 - removed favicon

--- header.php
+++ header.php
@@ -14,7 +14,6 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 <?php wp_head(); ?>
 </head>
 

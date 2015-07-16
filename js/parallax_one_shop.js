7df9924052fa524c4527843464ae4e8e6663377a
cristian-ungureanu <cristian@themeisle.com>
2015-07-16 19:01:50 +0300

    blogfix , shop fix, inputs fix

--- js/parallax_one_shop.js
+++ js/parallax_one_shop.js
@@ -5,10 +5,10 @@ jQuery(window).load(function () {
     jQuery(".single-product .upsells .products").gridalicious({selector: '.product', width: 250, gutter: 30,});
     jQuery(".single-product .upsells .products").show();
     
-    jQuery(".single-product .related .products").gridalicious({selector: '.product', width: 200, gutter: 30,});
+    jQuery(".single-product .related .products").gridalicious({selector: '.product', width: 250, gutter: 30,});
     jQuery(".single-product .related .products").show();
     
-    jQuery(".archive .products").gridalicious({selector: '.product', width: 250, gutter: 30,});
+    jQuery(".archive .products").gridalicious({selector: '.product', width: 250, gutter: 30});
     jQuery(".archive .products").show();
     
     jQuery("#reviews").show();

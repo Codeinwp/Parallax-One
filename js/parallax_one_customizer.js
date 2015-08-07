fcffa4ab5d89bb5a87dcc2ae15eef807747ae905
cristian-ungureanu <cristian@themeisle.com>
2015-08-07 14:48:25 +0300

    added parallax controls

--- js/parallax_one_customizer.js
+++ js/parallax_one_customizer.js
@@ -224,15 +224,28 @@ jQuery(document).ready(function(){
      }
   
  
- 
  });
- 
-  jQuery('#customize-control-parallax_one_default_contact_form_show_recaptcha').find('input:checkbox').on('change',function(){
-  if(jQuery(this).is(':checked')){
-   jQuery('#customize-control-parallax_one_recaptcha_sitekey').fadeOut();
-   jQuery('#customize-control-parallax_one_recaptacha_secretkey').fadeOut();
-  } else {
-   jQuery('#customize-control-parallax_one_recaptcha_sitekey').fadeIn();
-   jQuery('#customize-control-parallax_one_recaptacha_secretkey').fadeIn();
-  } });
+  
+	var sh = jQuery('#customize-control-paralax_one_enable_move').find('input:checkbox');
+	if(!sh.is(':checked')){
+		jQuery('#customize-control-paralax_one_first_layer').hide();
+		jQuery('#customize-control-paralax_one_second_layer').hide();
+		jQuery('#customize-control-header_image').show();
+	} else {
+		jQuery('#customize-control-paralax_one_first_layer').show();
+		jQuery('#customize-control-paralax_one_second_layer').show();
+		jQuery('#customize-control-header_image').hide();
+	}
+	
+	sh.on('change',function(){
+		if(jQuery(this).is(':checked')){
+			jQuery('#customize-control-paralax_one_first_layer').fadeIn();
+			jQuery('#customize-control-paralax_one_second_layer').fadeIn();
+			jQuery('#customize-control-header_image').fadeOut();
+		} else {
+			jQuery('#customize-control-paralax_one_first_layer').fadeOut();
+			jQuery('#customize-control-paralax_one_second_layer').fadeOut();
+			jQuery('#customize-control-header_image').fadeIn();
+		} 
+	});
 });

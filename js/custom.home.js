30acb6f273d1c3f82002de23bd37bb3a2cdb8ddb
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2015-08-12 17:00:17 +0300

    !!! Removed unused code and fixed Uncaught TypeError: Cannot read property 'offset' of undefined

--- js/custom.home.js
+++ js/custom.home.js
@@ -187,79 +187,7 @@ jQuery(document).ready(function(){
     }
 });
 
-
 // parallax effect
-/*
-jQuery( document ).ready( function( $ ) {
-
-    if( !isMobile.any() && jQuery('#parallax_move').length>0 ){
-        
-        var parallax_one_navigation_height  = jQuery('#sticky-navigation').outerHeight();
-        var parallax_one_bigTitle_height    = jQuery('#parallax_header').outerHeight();
-        $parallax_one_bigTitle_logo = jQuery('.only-logo');
-
-        var parallax_one_bigTitle_logo_height;
-        jQuery('.only-logo img').load(function(){
-            parallax_one_bigTitle_logo_height = $parallax_one_bigTitle_logo.height();
-            $parallax_one_bigTitle_logo.css({
-                'height':   parallax_one_bigTitle_logo_height,
-            });
-            $parallax_one_bigTitle_logo.find('.navbar').css({
-                'position':'absolute',
-                'left':    0,
-                'right':   0,
-                'z-index': 99,
-            });
-        });
-
-        jQuery(window).scroll(function () {
-            var parallax_one_scrollTop   = document.body.scrollTop;
-            var parallax_one_bigTitle_logo_position = $parallax_one_bigTitle_logo.offset().top + parallax_one_scrollTop - parallax_one_scrollTop/10;
-            document.getElementById('intro_section_text_1').style.opacity = 1 - (  parallax_one_scrollTop * 3 / parallax_one_bigTitle_height );
-            document.getElementById('intro_section_text_2').style.opacity = 1 - (  parallax_one_scrollTop * 2 / parallax_one_bigTitle_height );
-            var parallax_one_headerButtonOffset   = $('#intro_section_text_3').offset().top,
-                parallax_one_headerDistanceButton = (parallax_one_headerButtonOffset - parallax_one_scrollTop);
-            var parallax_one_headerLogoOffset     = $('#only-logo-inner').offset().top,
-                parallax_one_headerDistanceLogo   = (parallax_one_headerLogoOffset - parallax_one_scrollTop);
-            if ( parallax_one_headerDistanceButton-50 <= parallax_one_headerDistanceLogo+parallax_one_bigTitle_logo_height ) {
-                $('#intro_section_text_3').css({
-                    'height':   $('#intro_section_text_3').height(),
-                });
-                $('#inpage_scroll_btn').css({
-                    'position':'absolute',
-                    'left':    '0',
-                    'right':   '0',
-                });
-                $('.intro-section-text-wrap').css({
-                    'position': 'initial',
-                });
-                if ( parallax_one_bigTitle_height-parallax_one_scrollTop > parallax_one_navigation_height+parallax_one_bigTitle_logo_height+$('#intro_section_text_3').height()+250 ) {
-                    document.getElementById('inpage_scroll_btn').style.top = parallax_one_bigTitle_logo_position + 55 + parallax_one_bigTitle_logo_height + "px";
-                }
-            } else {
-                $('#intro_section_text_3').css({
-                    'height':   'auto',
-                });
-                $('#inpage_scroll_btn').css({
-                    'position':'relative',
-                    'left':    'auto',
-                    'right':   'auto',
-                    'top': 'auto',
-                });
-            }
-            if ( parallax_one_bigTitle_height-parallax_one_scrollTop > parallax_one_navigation_height+parallax_one_bigTitle_logo_height+$('#intro_section_text_3').height()+250 ) {
-                document.getElementById('only-logo-inner').style.top  = parallax_one_bigTitle_logo_position + "px"            
-            }
-        });
-
-    }
-
-});
-*/
-
-
-
-
 jQuery( document ).ready( function( $ ) {
 
     if( !isMobile.any() && jQuery('#parallax_move').length>0 ){
@@ -291,6 +219,7 @@ jQuery( document ).ready( function( $ ) {
     jQuery(window).scroll(function () {
 
         var parallax_one_scrollTop   = document.body.scrollTop;
+        $parallax_one_bigTitle_logo     = jQuery('.only-logo');
         var parallax_one_bigTitle_logo_position = $parallax_one_bigTitle_logo.offset().top + parallax_one_scrollTop - parallax_one_scrollTop/10;
         document.getElementById('intro_section_text_1').style.opacity = 1 - (  parallax_one_scrollTop * 3 / parallax_one_bigTitle_height );
         document.getElementById('intro_section_text_2').style.opacity = 1 - (  parallax_one_scrollTop * 2 / parallax_one_bigTitle_height );

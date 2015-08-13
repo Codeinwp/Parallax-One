62a70117d66a5d06a6a389ca23da7241e5663287
cristian-ungureanu <cristian@themeisle.com>
2015-08-13 12:36:07 +0300

    #224 - buttons issue

--- js/parallax_one_buttons_control.js
+++ js/parallax_one_buttons_control.js
@@ -1,7 +1,9 @@
 jQuery(document).ready(function() {
 	
-	jQuery('.wp-full-overlay-sidebar-content').prepend('<a style="width: 80%; margin: 5px auto 5px auto; display: block; text-align: center;" href="http://themeisle.com/forums/forum/parallax-one/" class="button" target="_blank">{support}</a>'.replace('{support}',objectL10n.support));
+	jQuery('#customize-theme-controls > ul').prepend('<li class="accordion-section parallax-upsells"></li>');
 	
-	jQuery('.wp-full-overlay-sidebar-content').prepend('<a style="width: 80%; margin: 5px auto 5px auto; display: block; text-align: center;" href="http://themeisle.com/documentation-parallax-one/" class="button" target="_blank">{documentation}</a>'.replace('{documentation}',objectL10n.documentation));
+	jQuery('.parallax-upsells').append('<a style="width: 80%; margin: 5px auto 5px auto; display: block; text-align: center;" href="http://themeisle.com/documentation-parallax-one/" class="button" target="_blank">{documentation}</a>'.replace('{documentation}',objectL10n.documentation));
+	
+	jQuery('.parallax-upsells').append('<a style="width: 80%; margin: 5px auto 5px auto; display: block; text-align: center;" href="http://themeisle.com/forums/forum/parallax-one/" class="button" target="_blank">{support}</a>'.replace('{support}',objectL10n.support));
 	
 });

faa12d6ea5899900fd344e80ddc669d6e3481595
cristian-ungureanu <cristian@themeisle.com>
2015-09-17 20:25:52 +0300

    !!!fixed js errors

--- js/custom.all.js
+++ js/custom.all.js
@@ -148,7 +148,7 @@ function mainNav() {
 function scrolled() {
 
     if ( jQuery(window).width() >= 751 ) {
-        var prallax-one_scrollTop = jQuery(window).scrollTop();       // cursor position
+        var prallax_one_scrollTop = jQuery(window).scrollTop();       // cursor position
         var headerHeight = jQuery('.sticky-navigation').outerHeight();   // header height
         var isInOneSection = 'no';                              // used for checking if the cursor is in one section or not
         // for all sections check if the cursor is inside a section
@@ -159,7 +159,7 @@ function scrolled() {
             var thisBegin   = parallax_one_offset - headerHeight;                      // where the section begins
             var thisEnd     = parallax_one_offset + thisHeight - headerHeight;         // where the section ends  
             // if position of the cursor is inside of the this section
-            if ( prallax-one_scrollTop >= thisBegin && prallax-one_scrollTop <= thisEnd ) {
+            if ( prallax_one_scrollTop >= thisBegin && prallax_one_scrollTop <= thisEnd ) {
                 isInOneSection = 'yes';
                 jQuery('.current').removeClass('current');
                 jQuery('#menu-primary a[href$="' + thisID + '"]').parent('li').addClass('current');    // find the menu button with the same ID section

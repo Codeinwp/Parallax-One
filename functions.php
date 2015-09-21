ceaeb9864933500dac25a7639ea54be4e0dae24b
cristian-ungureanu <cristian@themeisle.com>
2015-09-21 14:29:47 +0300

    !!!#259 - include font before style

--- functions.php
+++ functions.php
@@ -193,13 +193,13 @@ function parallax_one_wp_page_menu()
  * Enqueue scripts and styles.
  */
 function parallax_one_scripts() {
+	
+	wp_enqueue_style( 'parallax-one-font', '//fonts.googleapis.com/css?family=Cabin:400,600|Open+Sans:400,300,600');
 
 	wp_enqueue_style( 'parallax-one-bootstrap-style', parallax_get_file( '/css/bootstrap.min.css'),array(), '3.3.1');
 
 	wp_enqueue_style( 'parallax-one-style', get_stylesheet_uri(), array('parallax-one-bootstrap-style'),'1.0.0');
 	
-	wp_enqueue_style( 'parallax-one-font', '//fonts.googleapis.com/css?family=Cabin:400,600|Open+Sans:400,300,600');
-
 	wp_enqueue_script( 'parallax-one-bootstrap', parallax_get_file('/js/bootstrap.min.js'), array(), '3.3.5', true );
 		
 	wp_enqueue_script( 'parallax-one-custom-all', parallax_get_file('/js/custom.all.js'), array('jquery'), '2.0.1', true );

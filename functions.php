244cc7b6d426ba83c6289b7f16448f65fde17494
Marius Cristea <cristea.marius.alexandru@gmail.com>
2015-10-06 13:07:29 +0300

    !!! update

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function parallax_one_setup() {
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
 	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
+	 * provide it for us.  
 	 */
 	add_theme_support( 'title-tag' );
 

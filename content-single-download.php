a6594c188aa1d25fc792ddc41106cca9b8d890bc
claudiuvertistudio <claudiu.buruiana@codeinwp.com>
2015-11-25 16:48:54 +0200

    !!! Fixed EDD style

---
+++ content-single-download.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package parallax-one
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('content-single-page'); ?>>
+	<header class="entry-header single-header">
+		<?php the_title( '<h1 itemprop="headline" class="entry-title single-title">', '</h1>' ); ?>
+		<div class="colored-line-left"></div>
+		<div class="clearfix"></div>
+	</header><!-- .entry-header -->
+
+	<div itemprop="text" class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'parallax-one' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php parallax_one_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->

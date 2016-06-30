5965444dc42de5605674495c2a090d08ceec8a78
Cristian Ungureanu <cristian@themeisle.com>
2016-06-30 15:54:15 +0300

    #387 - remove required actions from functions.php and welcome-screen

--- functions.php
+++ functions.php
@@ -117,40 +117,6 @@ function parallax_one_setup() {
 	* Welcome screen
 	*/
 	if ( is_admin() ) {
-
-		global $parallax_one_required_actions;
-        /*
-         * id - unique id; required
-         * title
-         * description
-         * check - check for plugins (if installed)
-         * plugin_slug - the plugin's slug (used for installing the plugin)
-         *
-         */
-        $parallax_one_required_actions = array(
-			array(
-                "id" => 'parallax-one-req-ac-install-intergeo-maps',
-                "title" => esc_html__( 'Install Intergeo Maps - Google Maps Plugin' ,'parallax-one' ),
-                "description"=> esc_html__( 'In order to use map section, you need to install Intergeo Maps plugin then use it to create a map and paste the generated shortcode in Customize -> Contact section -> Map shortcode','parallax-one' ),
-                "check" => defined('INTERGEO_PLUGIN_NAME'),
-                "plugin_slug" => 'intergeo-maps'
-            ),
-            array(
-                "id" => 'parallax-one-req-ac-install-pirate-forms',
-                "title" => esc_html__( 'Install Pirate Forms' ,'parallax-one' ),
-                "description"=> esc_html__( 'Makes your contact page more engaging by creating a good-looking contact form on your website. The interaction with your visitors was never easier.','parallax-one' ),
-                "check" => defined('PIRATE_FORMS_VERSION'),
-                "plugin_slug" => 'pirate-forms'
-            ),
-			array(
-				"id" => 'parallax-one-req-ac-check-latest-posts',
-                "title" => esc_html__( 'Switch "Front page displays" to "Your latest posts" ' ,'parallax-one' ),
-                "description"=> esc_html__( 'In order to have the one page look for your website, please go to Customize -> Advanced options and switch "Front page displays" to "Your latest posts"','parallax-one' ),
-                "check" => parallax_one_is_not_post(),
-				"customizer" => true
-			)
-		);
-
 		require get_template_directory() . '/inc/admin/welcome-screen/welcome-screen.php';
 	}
 }
--- inc/admin/welcome-screen/welcome-screen.php
+++ inc/admin/welcome-screen/welcome-screen.php
@@ -26,11 +26,6 @@ class Parallax_One_Welcome {
 		add_action( 'parallax_one_welcome', array( $this, 'parallax_one_welcome_github' ), 		            20 );
 		add_action( 'parallax_one_welcome', array( $this, 'parallax_one_welcome_changelog' ), 				30 );
 		add_action( 'parallax_one_welcome', array( $this, 'parallax_one_welcome_free_pro' ), 				40 );
-		
-		/* ajax callback for dismissable required actions */
-		add_action( 'wp_ajax_parallax_one_dismiss_required_action', array( $this, 'parallax_one_dismiss_required_action_callback') );
-		add_action( 'wp_ajax_nopriv_parallax_one_dismiss_required_action', array($this, 'parallax_one_dismiss_required_action_callback') );
-
 	}
 
 	/**
@@ -72,30 +67,6 @@ class Parallax_One_Welcome {
 		if ( 'appearance_page_parallax-one-welcome' == $hook_suffix ) {
 			wp_enqueue_style( 'parallax-one-welcome-screen-css', get_template_directory_uri() . '/inc/admin/welcome-screen/css/welcome.css' );
 			wp_enqueue_script( 'parallax-one-welcome-screen-js', get_template_directory_uri() . '/inc/admin/welcome-screen/js/welcome.js', array('jquery') );
-			
-			global $parallax_one_required_actions;
-			
-			$nr_actions_required = 0;
-			
-			/* get number of required actions */
-			if( get_option('parallax_one_show_required_actions') ):
-				$parallax_one_show_required_actions = get_option('parallax_one_show_required_actions');
-			else:
-				$parallax_one_show_required_actions = array();
-			endif;
-			if( !empty($parallax_one_required_actions) ):
-				foreach( $parallax_one_required_actions as $parallax_one_required_action_value ):
-					if(( !isset( $parallax_one_required_action_value['check'] ) || ( isset($parallax_one_required_action_value['check'] ) && ( $parallax_one_required_action_value['check'] == false ) ) ) && ( (isset($parallax_one_show_required_actions[$parallax_one_required_action_value['id']]) && ($parallax_one_show_required_actions[$parallax_one_required_action_value['id']] == true)) || !isset($parallax_one_show_required_actions[$parallax_one_required_action_value['id']]) )) :
-						$nr_actions_required++;
-					endif;
-				endforeach;
-			endif;
-			wp_localize_script( 'parallax-one-welcome-screen-js', 'parallaxOneWelcomeScreenObject', array(
-				'nr_actions_required' => $nr_actions_required,
-				'ajaxurl' => admin_url( 'admin-ajax.php' ),
-				'template_directory' => get_template_directory_uri(),
-				'no_required_actions_text' => __( 'Hooray! There are no required actions for you right now.','parallax-one' )
-			) );
 		}
 	}
 	
@@ -106,63 +77,7 @@ class Parallax_One_Welcome {
 		
 		wp_enqueue_style( 'parallax-one-welcome-screen-customizer-css', get_template_directory_uri() . '/inc/admin/welcome-screen/css/welcome_customizer.css' );
 		wp_enqueue_script( 'parallax-one-welcome-screen-customizer-js', get_template_directory_uri() . '/inc/admin/welcome-screen/js/welcome_customizer.js', array('jquery'), '20120206', true );
-		
-		global $parallax_one_required_actions;
-		$nr_actions_required = 0;
-		/* get number of required actions */
-		if( get_option('parallax_one_show_required_actions') ):
-			$parallax_one_show_required_actions = get_option('parallax_one_show_required_actions');
-		else:
-			$parallax_one_show_required_actions = array();
-		endif;
-		if( !empty($parallax_one_required_actions) ):
-			foreach( $parallax_one_required_actions as $parallax_one_required_action_value ):
-				if(( !isset( $parallax_one_required_action_value['check'] ) || ( isset( $parallax_one_required_action_value['check'] ) && ( $parallax_one_required_action_value['check'] == false ) ) ) && ((isset($parallax_one_show_required_actions[$parallax_one_required_action_value['id']]) && ($parallax_one_show_required_actions[$parallax_one_required_action_value['id']] == true)) || !isset($parallax_one_show_required_actions[$parallax_one_required_action_value['id']]) )) :
-					$nr_actions_required++;
-				endif;
-			endforeach;
-		endif;
-		wp_localize_script( 'parallax-one-welcome-screen-customizer-js', 'parallaxOneWelcomeScreenCustomizerObject', array(
-			'nr_actions_required' => $nr_actions_required,
-			'aboutpage' => esc_url( admin_url( 'themes.php?page=parallax-one-welcome#actions_required' ) ),
-			'customizerpage' => esc_url( admin_url( 'customize.php#actions_required' ) ),
-			'themeinfo' => __('View Theme Info','parallax-one'),
-		) );
 	}
-	
-	/**
-	 * Dismiss required actions
-	 */
-	public function parallax_one_dismiss_required_action_callback() {
-		
-		global $parallax_one_required_actions;
-		
-		$parallax_one_dismiss_id = (isset($_GET['dismiss_id'])) ? $_GET['dismiss_id'] : 0;
-		echo $parallax_one_dismiss_id; /* this is needed and it's the id of the dismissable required action */
-		if( !empty($parallax_one_dismiss_id) ):
-			/* if the option exists, update the record for the specified id */
-			if( get_option('parallax_one_show_required_actions') ):
-				$parallax_one_show_required_actions = get_option('parallax_one_show_required_actions');
-				$parallax_one_show_required_actions[$parallax_one_dismiss_id] = false;
-				update_option( 'parallax_one_show_required_actions',$parallax_one_show_required_actions );
-			/* create the new option,with false for the specified id */
-			else:
-				$parallax_one_show_required_actions_new = array();
-				if( !empty($parallax_one_required_actions) ):
-					foreach( $parallax_one_required_actions as $parallax_one_required_action ):
-						if( $parallax_one_required_action['id'] == $parallax_one_dismiss_id ):
-							$parallax_one_show_required_actions_new[$parallax_one_required_action['id']] = false;
-						else:
-							$parallax_one_show_required_actions_new[$parallax_one_required_action['id']] = true;
-						endif;
-					endforeach;
-				update_option( 'parallax_one_show_required_actions', $parallax_one_show_required_actions_new );
-				endif;
-			endif;
-		endif;
-		die(); // this is required to return a proper result
-	}
-
 
 	/**
 	 * Welcome screen content

130d4468b38b8c389de2dbeb00f7b5f37e95f974
cristian-ungureanu <cristian@themeisle.com>
2016-11-14 11:54:21 +0200

    run grunt check

--- functions.php
+++ functions.php
@@ -614,19 +614,19 @@ if ( ! function_exists( 'parallax_one_general_repeater_is_empty' ) ) {
  *
  * @param string $template Template file name.
  */
-function parallax_one_get_template_part($template){
-	if(locate_template($template.'.php')) {
-		get_template_part($template);
+function parallax_one_get_template_part( $template ) {
+	if ( locate_template( $template . '.php' ) ) {
+		get_template_part( $template );
 	} else {
-		if(defined('PARALLAX_ONE_PLUS_PATH')){
-			$template = basename ($template);
-			if (get_template_directory() !== get_stylesheet_directory()) {
-				if(file_exists( get_stylesheet_directory() . '/sections/' . $template . '.php' )){
-					require_once ( get_stylesheet_directory() . '/sections/' . $template . '.php' );
+		if ( defined( 'PARALLAX_ONE_PLUS_PATH' ) ) {
+			$template = basename( $template );
+			if ( get_template_directory() !== get_stylesheet_directory() ) {
+				if ( file_exists( get_stylesheet_directory() . '/sections/' . $template . '.php' ) ) {
+					require_once( get_stylesheet_directory() . '/sections/' . $template . '.php' );
 					return;
 				}
 			}
-			if(file_exists ( PARALLAX_ONE_PLUS_PATH.'public/templates/'.$template.'.php' )) {
+			if ( file_exists( PARALLAX_ONE_PLUS_PATH . 'public/templates/' . $template . '.php' ) ) {
 				require_once( PARALLAX_ONE_PLUS_PATH . 'public/templates/' . $template . '.php' );
 			}
 		}

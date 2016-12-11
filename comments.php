4690a552c5c8a3dceb4b8faf1354e10f2ddd1418
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2016-12-12 00:18:36 +0200

    #445 fix issues with function not defined

--- comments.php
+++ comments.php
@@ -63,7 +63,6 @@ if ( post_password_required() ) {
 				wp_list_comments( array(
 					'style'       => 'ol',
 					'short_ping'  => true,
-					'callback'    => 'parallax_one_comment',
 					'avatar_size' => 60,
 				) );
 			?>

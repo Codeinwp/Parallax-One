99f25d72cc67f581f8fedf1635f0c127ee95961c
cristian-ungureanu <cristian@themeisle.com>
2017-04-24 13:25:48 +0300

    fix grunt check

--- inc/alpha-control/parallax-one-alpha-control.php
+++ inc/alpha-control/parallax-one-alpha-control.php
@@ -43,10 +43,13 @@ class Parallax_One_Customize_Alpha_Color_Control extends WP_Customize_Control {
 		$this->default = $this->setting->default;
 	}
 
-	public function enqueue(){
-        wp_enqueue_script( 'parallax-one-alpha-control', parallax_get_file( '/inc/alpha-control/js/script.js' ),array('jquery'), '1.0', true );
-        wp_enqueue_style( 'parallax-one-alpha-style', parallax_get_file( '/inc/alpha-control/css/style.css' ), '1.0' );
-    }
+    /**
+     * Enqueue scripts and style.
+     */
+	public function enqueue() {
+		wp_enqueue_script( 'parallax-one-alpha-control', parallax_get_file( '/inc/alpha-control/js/script.js' ),array( 'jquery' ), '1.0', true );
+		wp_enqueue_style( 'parallax-one-alpha-style', parallax_get_file( '/inc/alpha-control/css/style.css' ), '1.0' );
+	}
 
 	/**
 	 * The function to render the controler

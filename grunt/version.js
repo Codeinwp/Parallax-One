33ea7bc1a4e798aba4f6dc406565a99c45a966a1
Andrei Băicuș <baicusandrei@me.com>
2017-08-01 14:11:43 +0300

    Added grunt version task.

---
+++ grunt/version.js
@@ -0,0 +1,16 @@
+/* jshint node:true */
+//https://github.com/kswedberg/grunt-version
+module.exports = {
+    package: {
+        options: {
+            prefix: '"version"\\:\\s+"'
+        },
+        src: 'package.json'
+    },
+    style_ti: {
+        options: {
+            prefix: 'Version\\:\\s+'
+        },
+        src: 'style.css'
+    },
+};
